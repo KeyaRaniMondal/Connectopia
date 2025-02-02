54bfdf7aa0d32728c172fdeb72c55336b99b78ec
Sudip Kumar <keya24gadu2020@gmail.com>
2025-02-02 21:47:52 +0600

    implemented react type animation

--- src/PAGES/Home/home.jsx
+++ src/PAGES/Home/home.jsx
@@ -1,5 +1,6 @@
 import { DotLottieReact } from '@lottiefiles/dotlottie-react'
 import splashScreen from '../../assets/splashScreen.jpg'
+import { TypeAnimation } from 'react-type-animation'
 const Home = () => {
     return (
         <div style={
@@ -10,14 +11,35 @@ const Home = () => {
 
             }
         }>
+            <div className='flex gap-1'>
+                <div className='pt-32 w-[1000px]'>
+                    <DotLottieReact
+                        src={'Animation - 1738508469128.lottie'}
+                        loop
+                        autoplay
 
-            <div className='pt-32 w-[1000px]'>
-                <DotLottieReact
-                    src={'Animation - 1738508469128.lottie'}
-                    loop
-                    autoplay
+                    />
+                </div>
+                <div>
+                    <h2 className="text-6xl text-center mt-80 font-bold">Connectopia</h2>
+                    <p className='text-xl'> 
+                         <TypeAnimation
+                        sequence={[
+                            'Connect',
+                            1000,
+                            'Collaborate',
+                            1000,
+                            'Create',
+                            1000,
 
-                />
+                        ]}
+                        speed={50}
+                        style={{ fontSize: '2em' }}
+                        repeat={Infinity}/>
+                        </p>
+                </div>
+
+
             </div>
 
         </div>
