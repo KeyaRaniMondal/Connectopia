9311c13dd462e418620e7588e09c7967c30243fc
Sudip Kumar <keya24gadu2020@gmail.com>
2025-02-16 22:10:02 +0600

    created api to post users

--- Connectopia-Server/index.js
+++ Connectopia-Server/index.js
@@ -1,12 +1,12 @@
 // LW95nKB3nKM6046G
 // Connectopia
-const express=require('express')
-const cors=require('cors')
-const app=express()
+const express = require('express')
+const cors = require('cors')
+const app = express()
 require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const port=process.env.PORT||5000
-   const uri = `mongodb+srv://${process.env.db_USER}:${process.env.db_PASSWORD}@cluster0.nj8v5.mongodb.net/yourDatabaseName?retryWrites=true&w=majority&appName=Cluster0`;
+const port = process.env.PORT || 5000
+const uri = `mongodb+srv://${process.env.db_USER}:${process.env.db_PASSWORD}@cluster0.nj8v5.mongodb.net/yourDatabaseName?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -19,6 +19,15 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+
+    const usersCollection = client.db('ConnectopiaDB').collection('users')
+
+
+    app.post('/users', async (req, res) => {
+      const newUser = req.body;
+      const result = await usersCollection.insertOne(newUser)
+      res.send(result)
+    })
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
@@ -26,7 +35,7 @@ async function run() {
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
