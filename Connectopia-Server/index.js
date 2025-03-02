9843aba7646e986f6f1cb8beb7d065c9130b9613
Keya <keyamondal549@gmail.com>
2025-03-02 21:11:35 +0600

    used multer for uploading photos

--- Connectopia-Server/index.js
+++ Connectopia-Server/index.js
@@ -4,13 +4,30 @@ const app = express()
 require('dotenv').config();
 // multer for file uploads
 const multer=require('multer')
-const upload=multer({dest:'uploads/'})
+const fs=require('fs')
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000
 
 app.use(cors());
 app.use(express.json());
+app.use('/uploads',express.static('uploads'))
 
+
+const uploadDir = 'uploads';
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir);
+}
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/'); 
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + '-' + file.originalname); 
+  }
+});
+
+const upload = multer({ storage });
 
 const uri = `mongodb+srv://${process.env.db_USER}:${process.env.db_PASSWORD}@cluster0.nj8v5.mongodb.net/yourDatabaseName?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -52,13 +69,15 @@ async function run() {
 
 
     //creating posts by Users
+
     app.post('/createdPosts',upload.single('photo'), async (req, res) => {
       const { email,caption } = req.body
-      const photo=req.file
+      const photo=req.file?`/uploads/${req.file.filename}`:null
       const user = await usersCollection.findOne({ email })
       const newPost = {
-        email,caption,
-        photo:photo?photo.path:null,
+        email,
+        caption,
+        photo,
         createAt: new Date()
       }
       const result = await userPostsCollection.insertOne(newPost)
