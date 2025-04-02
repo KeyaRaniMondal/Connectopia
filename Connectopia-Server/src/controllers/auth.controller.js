76833c46866cd4366c5f4d80e0ddb3504dfbb219
Keya <keyamondal549@gmail.com>
2025-04-02 21:16:03 +0600

    created message schema

--- Connectopia-Server/src/controllers/auth.controller.js
+++ Connectopia-Server/src/controllers/auth.controller.js
@@ -99,7 +99,7 @@ export const checkAuth = (req, res) => {
     try {
         res.status(200).json(req.user)
     }
-    catch (err0r) {
+    catch (error) {
         console.log("error in checkAuth Controllar".error.message)
         res.status(500).json({ message: "internal server error" })
     }
--- Connectopia-Server/src/routes/auth.route.js
+++ Connectopia-Server/src/routes/auth.route.js
@@ -12,5 +12,5 @@ router.get('/logout',logout)
 
 router.put('/update-profile',protectRoute,updateProfile)
 
-router.get("/check",checkAuth)
+router.get("/check",protectRoute,checkAuth)
 export default router
