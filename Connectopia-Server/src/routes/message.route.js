ee6badfde219b6d9c04676c69fbbf9cb414d0ca7
Keya <keyamondal549@gmail.com>
2025-04-02 21:36:46 +0600

    created getmassage route

--- Connectopia-Server/src/routes/message.route.js
+++ Connectopia-Server/src/routes/message.route.js
@@ -1,8 +1,9 @@
 import express from 'express'
 import protectRoute from '../middleware/auth.middleware'
-import { getUserSideBar } from '../controllers/message.controller'
+import { getMessages, getUserSideBar } from '../controllers/message.controller'
 
 const router=express.Router()
 router.get('/users',protectRoute,getUserSideBar)
+router.get('/:id',protectRoute,getMessages)
 
 export default router
