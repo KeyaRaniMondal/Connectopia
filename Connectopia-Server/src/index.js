ae1da37f1c752a05d028659a192813bc7c0a4b74
Keya <keyamondal549@gmail.com>
2025-04-11 01:29:35 +0600

    added limit to upload images

--- Connectopia-Server/src/index.js
+++ Connectopia-Server/src/index.js
@@ -156,7 +156,7 @@
 //   console.log(`server running on port : ${port}`);
 // })
 
-import express from 'express';
+import express, { urlencoded } from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
@@ -170,7 +170,8 @@ const app = express();
 const PORT = process.env.PORT || 5001;
 
 // Middleware
-app.use(express.json()); 
+app.use(express.json({limit:"2mb"})); 
+app.use(express.urlencoded({extended:true,limit:"2mb"}))
 app.use(cookieParser());  
 app.use(cors({
     origin: "http://localhost:5173",
