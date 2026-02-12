0f3d5ff4d0e83a4fbfb62abddeb8f0162b1583c6
KeyaRaniMondal <keyamondal549@gmail.com>
2026-02-12 20:52:49 +0600

    updated

--- Connectopia-Frontend/src/lib/axios.js
+++ Connectopia-Frontend/src/lib/axios.js
@@ -12,9 +12,10 @@ import axios from "axios";
 const BASE_URL =
   import.meta.env.MODE === "development"
     ? "http://localhost:5001/api"
-    : "/api";
+    : import.meta.env.VITE_API_URL || "/api";
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
   withCredentials: true, // if server uses cookies
+  headers: { "Content-Type": "application/json" },
 });
